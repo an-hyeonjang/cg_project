bb9ae4c5905a165bd9081815d87fa286c75205b1
an-hyeonjang <an.hyeonjang94@gmail.com>
2019-09-22 18:31:41 +0900

    vector<circle_t> *parent 입력 실패

--- 1_movingCircle/src/circle.h
+++ 1_movingCircle/src/circle.h
@@ -2,9 +2,11 @@
 #ifndef __CIRCLE_H__
 #define __CIRCLE_H__
 
+#include <iostream>
+
 struct circle_t
 {
-	uint	index;
+	uint	collision[3] = { 0, FALSE, 0 }; // c1, c2, check collsion
 	vec2	center=vec2(0);		// 2D position for translation
 	float	radius=1.0f;		// radius
 	vec4	color;				// RGBA color in [0,1]
@@ -12,10 +14,10 @@ struct circle_t
 	vec2	velocity = vec2(0.1f);
 	float	mass = (float)pow(radius, 2.0) * PI;
 
-	uint	collision[2] = {0, FALSE}; // c1, c2, check collsion
+	std::vector<circle_t>* parent;
 
 	mat4	model_matrix;		// modeling transformation
-	void	update( float t );
+	void	update( float t ); 
 };
 
 float randnum(void) { return (float)(rand() - RAND_MAX / 2) / (RAND_MAX/2); }
@@ -27,7 +29,6 @@ bool checkCollision(circle_t a, circle_t b) { if (dis(a.center, b.center) <= a.r
 void checkCollision(std::vector<circle_t>& circles)
 {
 	printf("size:%u\n", circles.size());
-	int count = 1;
 	uint i, j;
 	for (j = 0; j < circles.size(); j++)
 	{
@@ -35,11 +36,10 @@ void checkCollision(std::vector<circle_t>& circles)
 		{
 			if (dis(circles[i].center, circles[j].center) <= circles[i].radius + circles[j].radius)
 			{
-				circles[i].collision[0] = j;
 				circles[i].collision[1] = TRUE;
+				circles[i].collision[2] = j;
 				printf("between%i, %i, dis:%f, radiss:%f, count:%i\n", i, j, dis(circles[i].center, circles[j].center), circles[i].radius + circles[j].radius, count);
-				printf("circle %u with %u, collsion%u\n", circles[i].index, circles[i].collision[0], circles[i].collision[1]);
-				count += 1;
+				printf("circle %u with %u, collsion\n", circles[i].collision[0], circles[i].collision[2]);
 			}
 		}
 	}
@@ -53,12 +53,15 @@ inline std::vector<circle_t> create_circles()
 	
 	for (uint i = 0; i < 5; i++)
 	{
-		c = { i, vec2(randnum(),randnum()),abs(randnum() / 2.0f),vec4(abs(randnum()),abs(randnum()),abs(randnum()),1.0f), vec2(randnum(),randnum()) };
-		//c.parent = circles;
-		//printf("test.index: %u", c.parent.at(1).index);
+		c = { {i , FALSE, 0 }, vec2(randnum(),randnum()),abs(randnum() / 2.0f),vec4(abs(randnum()),abs(randnum()),abs(randnum()),1.0f), vec2(randnum(),randnum()) };
 		circles.emplace_back(c);
+		c.parent = &circles;
+
+		std::cout << c.parent << std::endl;
 	}
+	for (auto k : circles) { std::cout << c.parent << std::endl; };
 	checkCollision(circles);
+	for (auto k : circles) { std::cout << k.parent << " " << k.collision[0] << k.collision[1] << k.collision[2] << std::endl; }
 	return circles;
 }
 
@@ -66,7 +69,15 @@ inline void circle_t::update(float t)
 {
 	vec2 center_u = velocity * t + center;
 
-	if (collision[1]) { caculate();  collision[1] = TRUE; }
+	if (collision[1]) 
+	{
+		uint j = collision[2];
+		std::cout << collision[0] << " " << parent << std::endl;
+		collision[1] = FALSE; 
+	}
+	else std::cout << collision[0] << " " << collision[2] << " not collsion" << std::endl;
+
+
 	// these transformations will be explained in later transformation lecture
 	mat4 scale_matrix =
 	{
